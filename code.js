a8625eb9212d9c79a34f353f2e97d3f6fe359a75
Hamza Darbani <76852492+Hamza-Dbn@users.noreply.github.com>
2022-08-08 11:27:39 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
-
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const HashTab = {};
+    for (let i = 0; i < s.length; i++) {
+        const chars = s[i];
+        HashTab[chars] = HashTab[chars] + 1 || 1;
+    }
+    for (let i = 0; i < s.length; i++) {
+        const chars = s[i];
+        if (HashTab[chars] === 1) {
+            return i;
+        }
+     }
+     return -1; 
+};
